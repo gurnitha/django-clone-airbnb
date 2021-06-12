5bea2237797f7b61fe09374d8bf9ef65ef8d1cca
ing <ingafter60@outlook.com>
2021-06-12 14:50:50 +0700

    3.0 Replacing Default User

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,10 @@
+# apps/users/models.py
+
+# Django modules
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    pass
